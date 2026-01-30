85a516330845e91a9bd85c85cd3630060a2dbe2e
samgcode <samgcode@gmail.com>
2026-01-30 15:04:30 -0700

    map parser
    + add map file parser
    # read map from file

--- game.gd
+++ game.gd
@@ -1,34 +1,37 @@
 extends Node2D
 
+@export
+var camera_smoothing: float = 0.03
+
 const UP = "up"
 const DOWN = "down"
 const LEFT = "left"
 const RIGHT = "right"
 
-const GROUND = "ma"
-const PLAYER = "soweli"
+var types = {}
 
-const init_map = [
-	[1,1,1,1,1,1,1,1,1,1,1,1],
-	[1,0,0,0,0,0,0,0,0,0,0,1],
-	[1,0,0,0,0,1,0,0,0,0,0,1],
-	[1,0,0,0,1,0,1,0,0,0,0,1],
-	[1,0,0,0,0,1,0,0,0,0,0,1],
-	[1,0,0,0,0,0,0,0,0,0,0,1],
-	[1,1,1,1,1,1,1,1,1,1,1,1],
-]
+var init_map;
+var map;
+var map_height;
+var map_width;
 
-var map = init_map.duplicate(true)
-var map_height = map.size()
-var map_width = map[0].size()
+var player;
 
 var player_pos: Vector2 = Vector2(2, 2)
+var camera_target: Vector2 = Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	map[player_pos.y][player_pos.x] = 2
-	reset_map()
-
+	var map_data = read_files.load_map("res://map.txt")
+	types = map_data["types"]
+	init_map = map_data["map"]
+	map = init_map.duplicate(true)
+	map_height = map.size()
+	map_width = map[0].size()
+	player = map_data["player"]
+	
+	map[player_pos.y][player_pos.x] = player
+	update_map()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -42,28 +45,28 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed(RIGHT):
 		input_dir.x = 1
 	update_player(input_dir)
+	
+	$Camera.position = lerp($Camera.position, camera_target, camera_smoothing)
 
 func update_player(input_dir: Vector2):
 	map[player_pos.y][player_pos.x] = init_map[player_pos.y][player_pos.x]
 	player_pos += input_dir
 	if map[player_pos.y][player_pos.x] != 0:
 		player_pos -= input_dir
-	$Camera.position = player_pos * 64 + Vector2(32, 32)
-	map[player_pos.y][player_pos.x] = 2
-	reset_map()
+	camera_target = player_pos * 64 + Vector2(32, 32)
+	map[player_pos.y][player_pos.x] = player
+	update_map()
 
-func reset_map():
+func update_map():
 	var map_text: String = ""
 	for row in range(0, map.size()):
 		for col in range(0, map[row].size()):
-			var item = map[row][col]
-			match item:
-				0: map_text += GROUND
-				1: map_text += "ijo"
-				2: map_text += PLAYER
-				_: 
-					map_text += "seme"
-					print("Symbol %s not defined at row %s, col %s" % [item, row, col])
+			var tile = map[row][col]
+			if types.has(tile):
+				map_text += types[tile]
+			else:
+				map_text += "seme"
+				print("Symbol %s not defined at row %s, col %s" % [tile, row, col])
 			map_text += "|"
 		map_text += "\n"
 	
---
+++ read_files.gd
@@ -0,0 +1,73 @@
+extends Node
+
+func load_map(path: String) -> Dictionary:
+	var file = FileAccess.open(path, FileAccess.READ)
+	var content = file.get_as_text()
+	var lines = content.split('\n');
+	
+	var types_start
+	var types_end
+	var map_start
+	var map_end
+	var properties = []
+	for i in range(0, lines.size()):
+		var line = lines[i]
+		line = replace_regex(line, r'\/\/.*', "") # remove comments
+		line = replace_regex(line, r'[ \t]', "") # remove whitespace
+		lines[i] = line
+		
+		match line:
+			"types:": types_start = i + 1
+			"/types": types_end = i
+			"map:": map_start = i + 1
+			"/map": map_end = i
+			"": continue
+			_: properties.append(i)
+		
+	
+	var types = {}
+	for i in range(types_start, types_end):
+		var line = split_regex(lines[i], r'=')
+		if !line[0].is_valid_int():
+			print("ERROR: non number value in map")
+		else:
+			types[line[0].to_int()] = line[1]
+	
+	var map = []
+	for i in range(map_start, map_end):
+		var line = lines[i].bigrams()
+		map.append([])
+		var y = i - map_start
+		for x in range(0, line.size()):
+			var tile = line[x]
+			if x % 2 == 0:
+				if !tile.is_valid_int():
+					print("ERROR: non number value in map")
+				else:
+					map[y].append(tile.to_int())
+	
+	var player
+	for i in properties:
+		if range(types_start, types_end).has(i) || range(map_start, map_end).has(i): continue
+		var line = lines[i].split(":")
+		match line[0]:
+			"player": player = line[1].to_int()
+			_: print("Property %s not defined" % line[0])
+	
+	return {
+		"map": map,
+		"types": types,
+		"player": player,
+	}
+
+# https://regexr.com/ (use PCRE engine)
+func replace_regex(string: String, regex: String, replacement: String) -> String:
+	var regex_ = RegEx.create_from_string(regex)
+	var string_ = string
+	for result in regex_.search_all(string):
+		string_ = string_.replace(result.get_string(), replacement)
+	return string_
+
+func split_regex(string: String, regex: String) -> Array:
+	var string_ = replace_regex(string, regex, 'ඞ')
+	return string_.split('ඞ')
